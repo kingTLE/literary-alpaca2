1c14bcd9595749d81159c2a448a27333a2f9a2d7
啊biu <104550635+kingTLE@users.noreply.github.com>
2023-10-16 00:21:08 +0800

    Add files via upload

--- chinese-tokenizer/text.py
+++ chinese-tokenizer/text.py
@@ -2,15 +2,14 @@ from transformers import LlamaTokenizer
 
 hf_dir = './incorporation_hf'
 Alpaca_dir = './Chinese-Alpaca-tokenizer'
-
+llama='./llama-2-13b-hf-tokenizer'
 text = """
-第一式龙争虎斗确实钢猛，似乎将一身之力灌注于刀身。这一刀下去开山裂石可能差一些，不过要把白云给劈成两半那是绰绰有余。
-　　眼看这一刀到了眼前，白云伸双手握住三叉戟接这一招。因为力道非常强大，所以就连白云也不敢怠慢。
-　　刀戟互碰之间，白云惊出了一身的冷汗。看似钢猛的一刀却毫无力度，就像是刀口轻轻的放到了戟身，就连互碰都称不上。但此时想要换招却并不容易。为了接王富贵这一招可是用了五成力道伸了双手往上顶，想不到这一下扑了个空，身体一下就不受控制往前倾。
-　　王富贵把握机会使出了第二式，剃鳞，第三式，卸甲。"""
-
-
-
+挟太山以超北海，语人曰：‘我不能。’是诚不能也。
+为长者折枝，语人曰：‘我不能。’是不为也，非不能也。
+故王之不王，非挟太山以超北海之类也；王之不王，是折枝之类也。
+老吾老，以及人之老；幼吾幼，以及人之幼：天下可运于掌。
+《诗》云：‘刑于寡妻，至于兄弟，以御于家邦。
+"""
 def test_tokenizers(tokenizer_dir, text):
     llama_tokenizer = LlamaTokenizer.from_pretrained(tokenizer_dir)
 
@@ -18,4 +17,5 @@ def test_tokenizers(tokenizer_dir, text):
 
 
 print("Literary-alpaca2 tokenizer 分词结果:", test_tokenizers(hf_dir,text))
+print("llama2原生 tokenizer 分词结果:", test_tokenizers(llama,text))
 print("Alpaca tokenizer 分词结果:",test_tokenizers(Alpaca_dir,text))
