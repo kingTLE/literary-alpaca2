cd5d2a65b01b007092ac11bc63c4875937215022
啊biu <104550635+kingTLE@users.noreply.github.com>
2023-11-15 17:34:11 +0800

    Update pretrain-peft1.py

--- train/GPU/pretrain-peft1.py
+++ train/GPU/pretrain-peft1.py
@@ -47,10 +47,7 @@ class DataTrainingArguments:
 
 @dataclass
 class MyTrainingArguments(TrainingArguments):
-    double_quant: Optional[bool] = field(default=True)
-    quant_type: Optional[str] = field(default="nf4")
-    load_in_kbits: Optional[int] = field(default=16)
-    max_steps: Optional[int] = field(default=47)
+    max_steps: Optional[int] = field(default=1000)
     load_best_model_at_end: Optional[bool] = field(default=False)
     output_dir: Optional[str] = field(default=tempfile.mkdtemp())
     run_name: Optional[str] = field(default=output_dir)
@@ -183,7 +180,7 @@ def main():
     )
     logger.info(f"训练参数 {training_args}")
     set_seed(training_args.seed)
-    # check_and_create_dir(training_args.output_dir)
+    check_and_create_dir(training_args.output_dir)
     last_checkpoint = None
     if os.path.isdir(training_args.output_dir):
         last_checkpoint = get_last_checkpoint(training_args.output_dir)
@@ -214,21 +211,7 @@ def main():
     # config = AutoConfig.from_pretrained(model_args.pretrained_model_name, pretraining_tp=1)
 
     print('开始加载模型')
-    compute_dtype = (torch.float16 if training_args.fp16 else (torch.bfloat16 if training_args.bf16 else torch.float32))
-    load_in_4bit = training_args.load_in_kbits == 4
-    load_in_8bit = training_args.load_in_kbits == 8
-    if training_args.load_in_kbits in [4, 8]:
-        quantization_config = BitsAndBytesConfig(
-            load_in_4bit=training_args.load_in_kbits == 4,
-            load_in_8bit=training_args.load_in_kbits == 8,
-            llm_int8_threshold=8.0,
-            bnb_4bit_compute_dtype=compute_dtype,
-            load_in_8bit_skip_modules='embed_tokens',
-            bnb_4bit_use_double_quant=training_args.double_quant,
-            bnb_4bit_quant_type=training_args.quant_type
-        )
-    # if quantization_config is not None:
-    #     logger.info(f"量化配置:{quantization_config.to_dict()}")
+
     model = AutoModelForCausalLM.from_pretrained(
         model_args.pretrained_model_name,
         # config=config,
@@ -237,64 +220,18 @@ def main():
         # token=training_args.hub_token,
         # torch_dtype=torch.bfloat16,
         torch_dtype=torch.float16,
-        device_map="auto",
-        load_in_4bit=load_in_4bit,
-        load_in_8bit=load_in_8bit,
-        # quantization_config=quantization_config,
     )
     print('加载模型结束')
-    if training_args.load_in_kbits in [4, 8]:
-        loaded_in_kbit = getattr(model, "is_loaded_in_8bit", False) or getattr(model, "is_loaded_in_4bit", False)
-        for name, param in model.named_parameters():
-            param.requires_grad = False
-        for param in model.parameters():
-            if ((param.dtype == torch.float16) or (param.dtype == torch.bfloat16)) and loaded_in_kbit:
-                param.data = param.data.to(torch.float32)
-        for name, module in model.named_modules():
-            if 'norm' in name:
-                module = module.to(torch.float32)
-        if loaded_in_kbit and training_args.gradient_checkpointing:
-            if hasattr(model, "enable_input_require_grads"):
-                model.enable_input_require_grads()
-            else:
-                def make_inputs_require_grad(module, _input, output):
-                    output.requires_grad_(True)
-                model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
-            model.gradient_checkpointing_enable()
     model.config.use_cache = False
     embedding_size = model.get_input_embeddings().weight.shape[0]
     if len(tokenizer) > embedding_size:
         model.resize_token_embeddings(len(tokenizer))
-    # model = prepare_model_for_kbit_training(model)
-    # peft_config = LoraConfig(
-    #     task_type='CAUSAL_LM',
-    #     inference_mode=False,
-    #     r=64, lora_alpha=128,
-    #     target_modules=['q_proj', 'v_proj', 'k_proj', 'o_proj', 'gate_proj', 'down_proj', 'up_proj'],
-    #     modules_to_save=["embed_tokens"],
-    #     lora_dropout=0.05)
-    # model = get_peft_model(model, peft_config)
     for name, param in model.named_parameters():
         if "model.embed_tokens" not in name:
             param.requires_grad = False
         else:
             param.requires_grad = True
-        print('\n requires_grad属性2:', name, param.requires_grad,"\n")
-    # for name, module in model.named_modules():
-    #     if isinstance(module, LoraLayer):
-    #         if training_args.bf16:
-    #             module = module.to(torch.bfloat16)
-    #         if training_args.fp16:
-    #             module = module.to(torch.float16)
-    #     if 'norm' in name:
-    #         module = module.to(torch.float16)
-    #     if 'embed_tokens' in name:
-    #         if hasattr(module, 'weight'):
-    #             if training_args.bf16 and module.weight.dtype == torch.float32:
-    #                 module = module.to(torch.bfloat16)
-    #             if training_args.fp16 and module.weight.dtype == torch.float32:
-    #                 module = module.to(torch.float16)
-    # model.print_trainable_parameters()
+        print('\n requires_grad属性:', name, param.requires_grad,"\n")
     trainer = init_trainer(model, training_args, train_dataset, tokenizer)
     # 添加早停回调
     # early_stopping_callback = EarlyStoppingCallback(early_stopping_patience=3)
