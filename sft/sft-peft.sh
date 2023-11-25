088f7a7d7548d9749d80d72f7162d355ebe5bdcd
啊biu <104550635+kingTLE@users.noreply.github.com>
2023-11-25 22:32:07 +0800

    Update sft-peft.sh

--- sft/sft-peft.sh
+++ sft/sft-peft.sh
@@ -1,11 +1,11 @@
-output_model=/root/autodl-tmp/lora
+output_model=/root/autodl-tmp/sft-lora
 model=/root/autodl-fs/train2/working
 
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
-torchrun --nnodes 1 --nproc_per_node 1 ./sft-peft.py \
-    --deepspeed ./deepspeed_config_sft.json \
-    --train_files ../data  \
-    --data_cache_dir ../data-cache \
+torchrun --nnodes 1 --nproc_per_node 1 /root/autodl-fs/train/sft-peft.py \
+    --deepspeed /root/autodl-fs/train/deepspeed_config_sft.json \
+    --data_cache_dir /root/autodl-tmp/data \
+    --train_files /root/autodl-fs/train/data  \
     --output_dir ${output_model} \
     --hub_token "" \
     --push_to_hub false \
@@ -16,16 +16,16 @@ torchrun --nnodes 1 --nproc_per_node 1 ./sft-peft.py \
     --load_best_model_at_end  True \
     --logging_first_step True \
     --lr_scheduler_type cosine \
-    --learning_rate 2e-5 \
+    --learning_rate 1e-4 \
     --use_fast_tokenizer false\
     --num_train_epochs 1 \
     --logging_dir '/root/tf-logs' \
     --logging_strategy steps \
     --logging_steps 1 \
     --save_steps 1000 \
-    --eval_steps 200  \
+    --eval_steps 250  \
     --save_total_limit 5 \
-    --seed 50 \
+    --seed 42 \
     --disable_tqdm false \
     --ddp_find_unused_parameters false \
     --block_size 4096 \
@@ -39,6 +39,4 @@ torchrun --nnodes 1 --nproc_per_node 1 ./sft-peft.py \
     | tee -a ${model}/trains.log
 
 #    --resume_from_checkpoint ${output_model}/checkpoint-20400 \
-    # --peft_path /root/autodl-tmp/sft-lora \
-
-
+    # --peft_path /root/autodl-tmp/lora \
